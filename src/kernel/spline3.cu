85f20dbe11e27fbcd8580b2fdb65ec3d39486f3f
meso272 <liujinyang1201@126.com>
2023-11-07 15:27:36 -0800

    debug

--- src/kernel/spline3.cu
+++ src/kernel/spline3.cu
@@ -94,6 +94,7 @@ int spline_construct(
    else
     intp_param.alpha=a5;
     if(intp_param.auto_tuning==1){
+     std::cout<<"att "<<intp_param.auto_tuning<<std::endl;
       cusz::c_spline3d_profiling_16x16x16data<T*, DEFAULT_BLOCK_SIZE>  //
         <<<auto_tuning_grid_dim, dim3(DEFAULT_BLOCK_SIZE, 1, 1), 0, (GpuStreamT)stream>>>(
             data->dptr(), data->template len3<dim3>(),
