ea6f0b6f88997603518e5093be93c50399a49a1b
shixun404 <shixun404.com>
2025-03-29 21:10:00 -0400

    remove src/include

--- include/port/hip_suppress_warning.hh
+++
@@ -1,5 +0,0 @@
-// below is given by rocthrust in HIP 5.5.
-
-#define __has_trivial_constructor __is_trivially_constructible
-#define __has_trivial_copy __is_trivially_copyable
-// #define __has_trivial_assign __is_trivially_assignable
