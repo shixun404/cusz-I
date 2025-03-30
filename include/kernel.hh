1b7c8556d857432c5be7bbb1074c103861d3cbfa
shixun404 <shixun404.com>
2025-03-29 21:10:46 -0400

    remove src/include

--- include/kernel.hh
+++
@@ -1,5 +0,0 @@
-#include "kernel/hist.hh"
-#include "kernel/histsp.hh"
-#include "kernel/lrz.hh"
-#include "kernel/spv.hh"
-#include "kernel/spline.hh"
--- include/rre/rre.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void TCMS_COMPRESS(void* input, size_t insize, uint8_t** output, size_t* outsize, size_t* tcms_padding_bytes, float* time);
-void TCMS_DECOMPRESS(uint8_t* input, void** output, size_t tcms_padding_bytes, float* time);
-void BITR_COMPRESS(void* input, size_t insize, uint8_t** output, size_t* outsize, size_t* bitr_padding_bytes, float* time);
-void BITR_DECOMPRESS(uint8_t* input, void** output, size_t bitr_padding_bytes, float* time);
